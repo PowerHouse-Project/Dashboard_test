bc7f0d44a3ed85ed3d92d28afe98c2d600f7c6be
Aditya Shibu <as2397@hw.ac.uk>
2024-10-29 08:49:34 +0400

    Configured fonts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    
+    <>
+      <h1 className="text-3xl font-bold font-jetBrainsExtraBold">
+        Hello, World!
+      </h1>
+
+      <h1 className="text-3xl font-bold font-jetBrains">Hello, World!</h1>
+    </>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetBrains: ["JetbrainsBold", "monospace"],
+        jetBrainsExtraBold: ["JetbrainsExtraBold", "monospace"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
