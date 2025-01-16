09c82db1a9d2f9023e827940d561f372f33ffaef
Aditya Shibu <as2397@hw.ac.uk>
2025-01-16 23:43:46 +0400

    Enhance PageTitle component with breadcrumb navigation and styling; add Dashboard component

--- src/components/Dashboard/PageTitle.jsx
+++ src/components/Dashboard/PageTitle.jsx
@@ -1,7 +1,23 @@
 import "../../css/pageTitle.css";
 
 const PageTitle = () => {
-  return <div>pageTitle</div>;
+  return (
+    <div className="pageTitle">
+      <h1 className="font-jetBrainsExtraBold text-main-light-blue-dark font-light">
+        Dashboard
+      </h1>
+      <nav>
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="/">
+              <i className="bi bi-house-door"></i>
+            </a>
+          </li>
+          <li className="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div>
+  );
 };
 
 export default PageTitle;
