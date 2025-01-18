208366fb38ee8e0a0ed2149ffebd438f950e6582
Aditya Shibu <as2397@hw.ac.uk>
2025-01-18 11:00:47 +0400

    Fetch and display device data in Dashboard component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,21 @@ import { useState, useEffect } from "react";
 import "../../css/dashboard.css";
 
 const Dashboard = () => {
+  const [devices, setDevices] = useState([]);
+
+  const fetchDevices = () => {
+    fetch("http://localhost:4000/devices")
+      .then((res) => res.json())
+      .then((data) => {
+        setDevices(data);
+      })
+      .catch((e) => console.log(e.message));
+  };
+
+  useEffect(() => {
+    fetchDevices();
+  }, []);
+
   return (
     <section className="dashboard section">
       <div className="row">
